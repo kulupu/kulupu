4f803eb7ebaf2c4e88efc57ee11313248f3b2ca2
Wei Tang <hi@that.world>
2019-09-20 09:50:16 +0200

    Fix genesis wasm

--- src/chain_spec.rs
+++ src/chain_spec.rs
@@ -101,7 +101,7 @@ fn testnet_genesis(
 fn mainnet_genesis() -> GenesisConfig {
 	GenesisConfig {
 		system: Some(SystemConfig {
-			code: WASM_BINARY.to_vec(),
+			code: include_bytes!("../res/genesis/kulupu_runtime.compact.wasm").to_vec(),
 			changes_trie_config: Default::default(),
 		}),
 		indices: Some(IndicesConfig {
