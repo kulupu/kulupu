1aa9c211ead8652deecbfc5e249b088aab19b57c
Wei Tang <hi@that.world>
2020-04-27 15:05:45 +0200

    Create pallet-eras crate for storing era information

---
+++ frame/eras/src/lib.rs
@@ -0,0 +1,45 @@
+// Copyright 2019-2020 Wei Tang.
+// This file is part of Kulupu.
+
+// Kulupu is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Kulupu is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Kulupu.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Era information recording.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use codec::{Encode, Decode};
+use frame_support::{decl_storage, decl_module};
+
+#[derive(Encode, Decode)]
+pub struct Era<H> {
+	/// Genesis block hash of the era.
+	pub genesis_block_hash: H,
+	/// Final block hash.
+	pub final_block_hash: H,
+	/// Final state root.
+	pub final_state_root: H,
+}
+
+pub trait Trait: frame_system::Trait { }
+
+decl_storage! {
+	trait Store for Module<T: Trait> as Eras {
+		/// Past eras.
+		pub PastEras get(fn past_eras) config(past_eras): Vec<Era<T::Hash>>;
+	}
+}
+
+decl_module! {
+	pub struct Module<T: Trait> for enum Call where origin: T::Origin { }
+}
