c08a50b90b283b0c5a991a9f34d10616178fcdbe
Wei Tang <hi@that.world>
2020-04-28 06:07:37 +0200

    Update Kulupu-specific PoW code

--- pow/src/lib.rs
+++ pow/src/lib.rs
@@ -1,14 +1,31 @@
+// Copyright 2019-2020 Wei Tang.
+// This file is part of Kulupu.
+
+// Kulupu is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Kulupu is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Kulupu.  If not, see <http://www.gnu.org/licenses/>.
+
 use std::sync::{Arc, Mutex};
 use std::cell::RefCell;
-use primitives::{U256, H256};
-use sr_primitives::generic::BlockId;
-use sr_primitives::traits::{
-	Block as BlockT, Header as HeaderT, ProvideRuntimeApi, UniqueSaturatedInto,
-};
-use client_api::{blockchain::HeaderBackend, backend::AuxStore};
 use codec::{Encode, Decode};
-use consensus_pow::PowAlgorithm;
-use consensus_pow_primitives::{Seal as RawSeal, DifficultyApi};
+use sp_core::{U256, H256};
+use sp_api::ProvideRuntimeApi;
+use sp_runtime::generic::BlockId;
+use sp_runtime::traits::{
+	Block as BlockT, Header as HeaderT, UniqueSaturatedInto,
+};
+use sp_consensus_pow::{Seal as RawSeal, DifficultyApi};
+use sc_consensus_pow::PowAlgorithm;
+use sc_client_api::{blockchain::HeaderBackend, backend::AuxStore};
 use kulupu_primitives::{Difficulty, AlgorithmApi, DAY_HEIGHT, HOUR_HEIGHT};
 use lru_cache::LruCache;
 use rand::{SeedableRng, thread_rng, rngs::SmallRng};
@@ -99,7 +116,7 @@ fn is_valid_hash(hash: &H256, difficulty: Difficulty) -> bool {
 fn key_hash<B, C>(
 	client: &C,
 	parent: &BlockId<B>
-) -> Result<H256, consensus_pow::Error<B>> where
+) -> Result<H256, sc_consensus_pow::Error<B>> where
 	B: BlockT<Hash=H256>,
 	C: HeaderBackend<B>,
 {
@@ -107,10 +124,10 @@ fn key_hash<B, C>(
 	const OFFSET: u64 = 2 * HOUR_HEIGHT;
 
 	let parent_header = client.header(parent.clone())
-		.map_err(|e| consensus_pow::Error::Environment(
+		.map_err(|e| sc_consensus_pow::Error::Environment(
 			format!("Client execution error: {:?}", e)
 		))?
-		.ok_or(consensus_pow::Error::Environment(
+		.ok_or(sc_consensus_pow::Error::Environment(
 			"Parent header not found".to_string()
 		))?;
 	let parent_number = UniqueSaturatedInto::<u64>::unique_saturated_into(*parent_header.number());
@@ -123,10 +140,10 @@ fn key_hash<B, C>(
 	let mut current = parent_header;
 	while UniqueSaturatedInto::<u64>::unique_saturated_into(*current.number()) != key_number {
 		current = client.header(BlockId::Hash(*current.parent_hash()))
-			.map_err(|e| consensus_pow::Error::Environment(
+			.map_err(|e| sc_consensus_pow::Error::Environment(
 				format!("Client execution error: {:?}", e)
 			))?
-			.ok_or(consensus_pow::Error::Environment(
+			.ok_or(sc_consensus_pow::Error::Environment(
 				format!("Block with hash {:?} not found", current.hash())
 			))?;
 	}
@@ -145,14 +162,14 @@ impl<C> RandomXAlgorithm<C> {
 }
 
 impl<B: BlockT<Hash=H256>, C> PowAlgorithm<B> for RandomXAlgorithm<C> where
-	C: HeaderBackend<B> + AuxStore + ProvideRuntimeApi,
+	C: HeaderBackend<B> + AuxStore + ProvideRuntimeApi<B>,
 	C::Api: DifficultyApi<B, Difficulty> + AlgorithmApi<B>,
 {
 	type Difficulty = Difficulty;
 
-	fn difficulty(&self, parent: &BlockId<B>) -> Result<Difficulty, consensus_pow::Error<B>> {
-		let difficulty = self.client.runtime_api().difficulty(parent)
-			.map_err(|e| consensus_pow::Error::Environment(
+	fn difficulty(&self, parent: H256) -> Result<Difficulty, sc_consensus_pow::Error<B>> {
+		let difficulty = self.client.runtime_api().difficulty(&BlockId::Hash(parent))
+			.map_err(|e| sc_consensus_pow::Error::Environment(
 				format!("Fetching difficulty from runtime failed: {:?}", e)
 			));
 
@@ -165,12 +182,14 @@ impl<B: BlockT<Hash=H256>, C> PowAlgorithm<B> for RandomXAlgorithm<C> where
 		pre_hash: &H256,
 		seal: &RawSeal,
 		difficulty: Difficulty,
-	) -> Result<bool, consensus_pow::Error<B>> {
-		assert_eq!(self.client.runtime_api().identifier(parent)
-				   .map_err(|e| consensus_pow::Error::Environment(
-					   format!("Fetching identifier from runtime failed: {:?}", e))
-				   )?,
-				   kulupu_primitives::ALGORITHM_IDENTIFIER);
+	) -> Result<bool, sc_consensus_pow::Error<B>> {
+		assert_eq!(
+			self.client.runtime_api().identifier(parent)
+				.map_err(|e| sc_consensus_pow::Error::Environment(
+					format!("Fetching identifier from runtime failed: {:?}", e))
+				)?,
+			kulupu_primitives::ALGORITHM_IDENTIFIER
+		);
 
 		let key_hash = key_hash(self.client.as_ref(), parent)?;
 
@@ -203,9 +222,9 @@ impl<B: BlockT<Hash=H256>, C> PowAlgorithm<B> for RandomXAlgorithm<C> where
 		pre_hash: &H256,
 		difficulty: Difficulty,
 		round: u32,
-	) -> Result<Option<RawSeal>, consensus_pow::Error<B>> {
+	) -> Result<Option<RawSeal>, sc_consensus_pow::Error<B>> {
 		let mut rng = SmallRng::from_rng(&mut thread_rng())
-			.map_err(|e| consensus_pow::Error::Environment(
+			.map_err(|e| sc_consensus_pow::Error::Environment(
 				format!("Initialize RNG failed for mining: {:?}", e)
 			))?;
 		let key_hash = key_hash(self.client.as_ref(), parent)?;
