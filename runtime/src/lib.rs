7a2a37a03de5df91ffd66370e8f35baf226dd8c1
Wei Tang <hi@that.world>
2020-04-30 01:30:37 +0200

    Shorter impl_name in runtime as well

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -97,7 +97,7 @@ pub mod opaque {
 /// This runtime version.
 pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kulupu"),
-	impl_name: create_runtime_str!("kulupu-substrate"),
+	impl_name: create_runtime_str!("kulupu"),
 	authoring_version: 2,
 	spec_version: 3,
 	impl_version: 0,
