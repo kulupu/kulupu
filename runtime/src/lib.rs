20dfcd6971cd2b8bd26ee0cbdbf90d7da20ded2b
Wei Tang <wei@that.world>
2020-08-12 07:31:59 +0200

    Increase spec_version to 7 and authoring_version to 4
    Bump of authoring_version will help with the check_inherents problem in the future.

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -110,8 +110,8 @@ pub mod opaque {
 pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kulupu"),
 	impl_name: create_runtime_str!("kulupu"),
-	authoring_version: 3,
-	spec_version: 6,
+	authoring_version: 4,
+	spec_version: 7,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
