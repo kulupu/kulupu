d33ba58bdc7805ddae31245eaa5e64af9b11f75b
Wei Tang <wei@that.world>
2020-08-22 02:20:45 +0200

    randomx: use generic to declare cache mode

--- pow/randomx/src/lib.rs
+++ pow/randomx/src/lib.rs
@@ -1,20 +1,52 @@
 use std::sync::Arc;
+use std::marker::PhantomData;
 
 pub const HASH_SIZE: usize = sys::RANDOMX_HASH_SIZE as usize;
 
-pub struct FullCache {
-	cache_ptr: *mut sys::randomx_cache,
-	dataset_ptr: *mut sys::randomx_dataset,
+pub enum CacheMode {
+	Full,
+	Light,
 }
 
-unsafe impl Send for FullCache { }
-unsafe impl Sync for FullCache { }
+pub unsafe trait WithCacheMode {
+	fn has_dataset() -> bool;
+	fn randomx_flags() -> sys::randomx_flags;
+}
 
-impl FullCache {
-	pub fn new(key: &[u8]) -> Self {
-		let flags = sys::randomx_flags_RANDOMX_FLAG_DEFAULT
+pub enum WithFullCacheMode { }
+unsafe impl WithCacheMode for WithFullCacheMode {
+	fn has_dataset() -> bool { true }
+	fn randomx_flags() -> sys::randomx_flags {
+		sys::randomx_flags_RANDOMX_FLAG_DEFAULT
 			| sys::randomx_flags_RANDOMX_FLAG_JIT
-			| sys::randomx_flags_RANDOMX_FLAG_FULL_MEM;
+			| sys::randomx_flags_RANDOMX_FLAG_FULL_MEM
+	}
+}
+
+pub enum WithLightCacheMode { }
+unsafe impl WithCacheMode for WithLightCacheMode {
+	fn has_dataset() -> bool { false }
+	fn randomx_flags() -> sys::randomx_flags {
+		sys::randomx_flags_RANDOMX_FLAG_DEFAULT
+			| sys::randomx_flags_RANDOMX_FLAG_JIT
+	}
+}
+
+pub struct Cache<M: WithCacheMode> {
+	cache_ptr: *mut sys::randomx_cache,
+	dataset_ptr: Option<*mut sys::randomx_dataset>,
+	_marker: PhantomData<M>,
+}
+
+pub type FullCache = Cache<WithFullCacheMode>;
+pub type LightCache = Cache<WithLightCacheMode>;
+
+unsafe impl<M: WithCacheMode> Send for Cache<M> { }
+unsafe impl<M: WithCacheMode> Sync for Cache<M> { }
+
+impl<M: WithCacheMode> Cache<M> {
+	pub fn new(key: &[u8]) -> Self {
+		let flags = M::randomx_flags();
 
 		let cache_ptr = unsafe {
 			let ptr = sys::randomx_alloc_cache(flags);
@@ -27,42 +59,52 @@ impl FullCache {
 			ptr
 		};
 
-		let dataset_ptr = unsafe {
-			let ptr = sys::randomx_alloc_dataset(flags);
-			let count = sys::randomx_dataset_item_count();
-			sys::randomx_init_dataset(ptr, cache_ptr, 0, count);
-			ptr
+		let dataset_ptr = if M::has_dataset() {
+			Some(unsafe {
+				let ptr = sys::randomx_alloc_dataset(flags);
+				let count = sys::randomx_dataset_item_count();
+				sys::randomx_init_dataset(ptr, cache_ptr, 0, count);
+				ptr
+			})
+		} else {
+			None
 		};
 
-		Self { cache_ptr, dataset_ptr }
+		Self { cache_ptr, dataset_ptr, _marker: PhantomData }
 	}
 }
 
-impl Drop for FullCache {
+impl<M: WithCacheMode> Drop for Cache<M> {
 	fn drop(&mut self) {
 		unsafe {
 			sys::randomx_release_cache(self.cache_ptr);
-			sys::randomx_release_dataset(self.dataset_ptr);
+		}
+
+		if M::has_dataset() {
+			unsafe {
+				sys::randomx_release_dataset(self.dataset_ptr.expect("Dataset was created"));
+			}
 		}
 	}
 }
 
-pub struct FullVM {
-	_cache: Arc<FullCache>,
+pub struct VM<M: WithCacheMode> {
+	_cache: Arc<Cache<M>>,
 	ptr: *mut sys::randomx_vm,
 }
 
-impl FullVM {
-	pub fn new(cache: Arc<FullCache>) -> Self {
-		let flags = sys::randomx_flags_RANDOMX_FLAG_DEFAULT
-			| sys::randomx_flags_RANDOMX_FLAG_JIT
-			| sys::randomx_flags_RANDOMX_FLAG_FULL_MEM;
+pub type FullVM = VM<WithFullCacheMode>;
+pub type LightVM = VM<WithLightCacheMode>;
+
+impl<M: WithCacheMode> VM<M> {
+	pub fn new(cache: Arc<Cache<M>>) -> Self {
+		let flags = M::randomx_flags();
 
 		let ptr = unsafe {
 			sys::randomx_create_vm(
 				flags,
 				cache.cache_ptr,
-				cache.dataset_ptr
+				cache.dataset_ptr.unwrap_or(std::ptr::null_mut()),
 			)
 		};
 
@@ -85,66 +127,9 @@ impl FullVM {
 	}
 }
 
-impl Drop for FullVM {
+impl<M: WithCacheMode> Drop for VM<M> {
 	fn drop(&mut self) {
 		unsafe {
-			sys::randomx_destroy_vm(self.ptr);
-		}
-	}
-}
-
-pub struct VM {
-	cache_ptr: *mut sys::randomx_cache,
-	ptr: *mut sys::randomx_vm,
-}
-
-impl VM {
-	pub fn new(key: &[u8]) -> Self {
-		let flags = sys::randomx_flags_RANDOMX_FLAG_DEFAULT
-			| sys::randomx_flags_RANDOMX_FLAG_JIT;
-
-		let cache_ptr = unsafe {
-			let ptr = sys::randomx_alloc_cache(flags);
-			sys::randomx_init_cache(
-				ptr,
-				key.as_ptr() as *const std::ffi::c_void,
-				key.len()
-			);
-
-			ptr
-		};
-
-		let ptr = unsafe {
-			sys::randomx_create_vm(
-				flags,
-				cache_ptr,
-				std::ptr::null_mut()
-			)
-		};
-
-		Self { cache_ptr, ptr }
-	}
-
-	pub fn calculate(&mut self, input: &[u8]) -> [u8; HASH_SIZE] {
-		let ret = [0u8; HASH_SIZE];
-
-		unsafe {
-			sys::randomx_calculate_hash(
-				self.ptr,
-				input.as_ptr() as *const std::ffi::c_void,
-				input.len(),
-				ret.as_ptr() as *mut std::ffi::c_void,
-			);
-		}
-
-		ret
-	}
-}
-
-impl Drop for VM {
-	fn drop(&mut self) {
-		unsafe {
-			sys::randomx_release_cache(self.cache_ptr);
 			sys::randomx_destroy_vm(self.ptr);
 		}
 	}
@@ -155,18 +140,20 @@ mod tests {
 	use super::*;
 
 	#[test]
-	fn should_create_vm() {
-		let mut vm = VM::new(&b"RandomX example key"[..]);
+	fn should_create_light_vm() {
+		let cache = Arc::new(LightCache::new(&b"RandomX example key"[..]));
+		let mut vm = LightVM::new(cache);
 		let hash = vm.calculate(&b"RandomX example input"[..]);
 		assert_eq!(hash, [69, 167, 169, 170, 66, 104, 77, 15, 73, 13, 233, 6, 227, 92, 143, 244, 95, 153, 4, 251, 223, 169, 78, 126, 236, 216, 174, 147, 1, 213, 223, 59]);
 	}
 
 	#[test]
 	fn should_work_with_full_vm() {
-		let mut vm = VM::new(&b"RandomX example key"[..]);
-		let hash = vm.calculate(&b"RandomX example input"[..]);
-		let cache = Arc::new(FullCache::new(&b"RandomX example key"[..]));
-		let mut full_vm = FullVM::new(cache);
+		let light_cache = Arc::new(LightCache::new(&b"RandomX example key"[..]));
+		let mut light_vm = LightVM::new(light_cache);
+		let hash = light_vm.calculate(&b"RandomX example input"[..]);
+		let full_cache = Arc::new(FullCache::new(&b"RandomX example key"[..]));
+		let mut full_vm = FullVM::new(full_cache);
 		let full_hash = full_vm.calculate(&b"RandomX example input"[..]);
 		assert_eq!(hash, full_hash);
 	}
