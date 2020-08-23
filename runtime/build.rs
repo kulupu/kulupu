c08af246dd917043b7f00144a2cca4ad0bdc485f
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-08-24 01:45:21 +0200

    still broken

--- runtime/build.rs
+++ runtime/build.rs
@@ -19,7 +19,7 @@ use wasm_builder_runner::WasmBuilder;
 fn main() {
 	WasmBuilder::new()
 		.with_current_project()
-		.with_wasm_builder_from_git("https://github.com/paritytech/substrate.git", "8c672e107789ed10973d937ba8cac245404377e2")
+		.with_wasm_builder_from_crates("2.0.0")
 		.export_heap_base()
 		.import_memory()
 		.build()
