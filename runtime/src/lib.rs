018b8f2fc10918d07730bb149888999ea4dc771f
Wei Tang <wei@that.world>
2020-08-29 21:53:15 +0200

    Bump spec version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -111,7 +111,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kulupu"),
 	impl_name: create_runtime_str!("kulupu"),
 	authoring_version: 5,
-	spec_version: 9,
+	spec_version: 11,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 6,
