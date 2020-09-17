4b1d536ca8a111ba4bad86524843a4e1c545561c
Wei Tang <wei@that.world>
2020-09-17 00:16:27 +0000

    Use vendored wasm builder to display error message

--- runtime/build.rs
+++ runtime/build.rs
@@ -19,7 +19,7 @@ use wasm_builder_runner::WasmBuilder;
 fn main() {
 	WasmBuilder::new()
 		.with_current_project()
-		.with_wasm_builder_from_crates("2.0.0")
+		.with_wasm_builder_from_crates_or_path("2.0.0", "../vendor/substrate/utils/wasm-builder")
 		.export_heap_base()
 		.import_memory()
 		.build()
