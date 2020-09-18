12438f4e67e425d9fb59b70c7abb227290a4ac8c
Wei Tang <wei@that.world>
2020-09-18 14:47:48 +0200

    Standardize all copyright headers

--- src/command.rs
+++ src/command.rs
@@ -1,18 +1,20 @@
-// Copyright 2019-2020 Wei Tang.
+// SPDX-License-Identifier: GPL-3.0-or-later
 // This file is part of Kulupu.
-
+//
+// Copyright (c) 2019-2020 Wei Tang.
+//
 // Kulupu is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-
+//
 // Kulupu is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-
+//
 // You should have received a copy of the GNU General Public License
-// along with Kulupu.  If not, see <http://www.gnu.org/licenses/>.
+// along with Kulupu. If not, see <http://www.gnu.org/licenses/>.
 
 use std::{path::PathBuf, fs::File, io::Write};
 use log::info;
