80913131af0e7d47f0a047c4aabcb260e7205a61
Wei Tang <wei@that.world>
2020-12-12 17:31:01 +0100

    Add EnsureRoot to all collective ensure proportion origins

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -589,8 +589,14 @@ parameter_types! {
 
 impl treasury::Trait for Runtime {
 	type Currency = Balances;
-	type ApproveOrigin = collective::EnsureProportionMoreThan<_4, _5, AccountId, CouncilCollective>;
-	type RejectOrigin = collective::EnsureProportionMoreThan<_1, _2, AccountId, CouncilCollective>;
+	type ApproveOrigin = system::EnsureOneOf<AccountId,
+		collective::EnsureProportionMoreThan<_4, _5, AccountId, CouncilCollective>,
+		system::EnsureRoot<AccountId>,
+	>;
+	type RejectOrigin = system::EnsureOneOf<AccountId,
+		collective::EnsureProportionMoreThan<_1, _2, AccountId, CouncilCollective>,
+		system::EnsureRoot<AccountId>,
+	>;
 	type Tippers = ElectionsPhragmen;
 	type TipCountdown = TipCountdown;
 	type TipFindersFee = TipFindersFee;
