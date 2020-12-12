3394fbfd2db755e867a51f0eeca92d9e8b697d78
Wei Tang <wei@that.world>
2020-12-12 17:27:31 +0100

    Reduce desired council member count to 7

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -516,7 +516,7 @@ parameter_types! {
 	pub const VotingBond: Balance = 5 * CENTS;
 	/// Daily council elections.
 	pub const TermDuration: BlockNumber = 24 * HOURS;
-	pub const DesiredMembers: u32 = 11;
+	pub const DesiredMembers: u32 = 7;
 	pub const DesiredRunnersUp: u32 = 30;
 	pub const ElectionsPhragmenModuleId: LockIdentifier = *b"phrelect";
 }
