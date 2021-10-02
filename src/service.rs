71885d00b40c047b26c5f13a2dc0ca8a0e73006d
Wei Tang <wei@that.world>
2021-10-02 02:30:24 +0200

    Switch from AlwaysCanAuthor to CanAuthorWithNativeVersion (#160)
    * Switch from AlwaysCanAuthor to CanAuthorWithNativeVersion
    
    * Fix cargo fmt

--- src/service.rs
+++ src/service.rs
@@ -26,7 +26,7 @@ use kulupu_pow::Error as PowError;
 use kulupu_runtime::{self, opaque::Block, RuntimeApi};
 use log::*;
 use parking_lot::Mutex;
-use sc_client_api::backend::RemoteBackend;
+use sc_client_api::{backend::RemoteBackend, ExecutorProvider};
 use sc_consensus::DefaultImportQueue;
 use sc_executor::NativeElseWasmExecutor;
 use sc_service::{error::Error as ServiceError, Configuration, TaskManager};
@@ -138,7 +138,13 @@ type PowBlockImport = sc_consensus_pow::PowBlockImport<
 	FullClient,
 	FullSelectChain,
 	kulupu_pow::RandomXAlgorithm<FullClient>,
-	sp_consensus::AlwaysCanAuthor,
+	sp_consensus::CanAuthorWithNativeVersion<
+		sc_service::LocalCallExecutor<
+			Block,
+			sc_client_db::Backend<Block>,
+			NativeElseWasmExecutor<ExecutorDispatch>,
+		>,
+	>,
 	CreateInherentDataProviders,
 >;
 
@@ -214,7 +220,7 @@ pub fn new_partial(
 		check_inherents_after,
 		select_chain.clone(),
 		CreateInherentDataProviders,
-		sp_consensus::AlwaysCanAuthor,
+		sp_consensus::CanAuthorWithNativeVersion::new(client.executor().clone()),
 	);
 
 	let import_queue = sc_consensus_pow::import_queue(
@@ -337,7 +343,7 @@ pub fn new_full(
 			CreateInherentDataProviders,
 			Duration::new(10, 0),
 			Duration::new(10, 0),
-			sp_consensus::AlwaysCanAuthor,
+			sp_consensus::CanAuthorWithNativeVersion::new(client.executor().clone()),
 		);
 		task_manager
 			.spawn_handle()
@@ -462,7 +468,7 @@ pub fn new_light(
 		check_inherents_after,
 		select_chain.clone(),
 		CreateInherentDataProviders,
-		sp_consensus::AlwaysCanAuthor,
+		sp_consensus::NeverCanAuthor,
 	);
 
 	let import_queue = sc_consensus_pow::import_queue(
