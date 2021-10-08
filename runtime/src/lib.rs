0f89b1306803e4c7822f332dcdfeaeca7ccc6c6c
Wei Tang <wei@that.world>
2021-10-08 16:34:26 +0200

    Release v2.9.0 (#167)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -129,7 +129,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kulupu"),
 	impl_name: create_runtime_str!("kulupu"),
 	authoring_version: 5,
-	spec_version: 27,
+	spec_version: 28,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 10,
